d8d836a633548d318ab88a53cedcc47c4c474e8d
andigarcia901 <119568800+andigarcia901@users.noreply.github.com>
2023-06-26 21:00:26 -1000

    Corrected and final .py file

--- average.py
+++ average.py
@@ -3,19 +3,22 @@
 
 def get_grade(s1, s2, s3):
     average = (s1 + s2 + s3)/3.0
-    if average <= 100 or average >= 90:
+    if average >= 90:
         return "A"
-    # elif 80 >= average <= 90:
-    #     return "B"
-    # elif 70>= average <= 80:
-    #     return "C"
-    # elif 60 >= average <= 70:
-    #     return "D"
+    elif average >= 80:
+        return "B"
+    elif average >= 70:
+        return "C"
+    elif average >= 60:
+        return "D"
     else: 
        return "F"
     
-student_one = get_grade(90, 95, 97)
+student_one = get_grade(90, 95, 97) # average is 94, should print A
 print(student_one)
 
-student_two = get_grade(88, 75, 91)
+student_two = get_grade(88, 75, 91) # average is 84.6, should print B
 print(student_two)
+
+student_three = get_grade(63, 86, 72) 
+print(student_three)
